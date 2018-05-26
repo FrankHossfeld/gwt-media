eaa27102b73dada7fb0daa5001205b184bf49fbe
Ahmad Bawaneh <akabme@gmail.com>
2018-05-26 17:05:08 +0300

    initial commit

---
+++ src/test/java/org/gwtproject/media/client/MediaSuite.java
@@ -0,0 +1,10 @@
+package org.gwtproject.media.client;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+  MediaTest.class
+})
+public class MediaSuite {}
---
+++ src/test/java/org/gwtproject/media/client/MediaTest.java
@@ -0,0 +1,12 @@
+package org.gwtproject.media.client;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+public class MediaTest extends GWTTestCase {
+    @Override
+    public String getModuleName() {
+        return "org.gwtproject.media.client.MediaTest";
+    }
+
+
+}
